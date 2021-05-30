aba55608461b4c30bffbc94ab626bb19349c794b
sohakhan21 <85063047+sohakhan21@users.noreply.github.com>
2021-05-30 18:04:41 +0500

    first commit

---
+++ pythonidle.py
@@ -0,0 +1,23 @@
+a= int(input("Enter the number: "))
+b= a+3
+print("After Adding 3 =",b)
+c= b*2
+print("After Multiplying by 2 =",c)
+d= c-4
+print("Afer Subtracting 4 =",d)
+e= d+3
+print("Again Adding 3 =",e)
+
+Celsius= int(input("Enter the number in Celsius: "))
+Fahrenheit= ((Celsius*1.8)+32)
+print("The value of Fahrenhiet is",(Fahrenheit))
+
+a= "\tHello\tWorld\t"
+print(a)
+b= "\nHello\nWorld\n"
+print(b)
+
+str= "Hello World"
+print(str.lstrip('H'))
+print(str.rstrip('l d'))
+print(str.strip('H e l'))
